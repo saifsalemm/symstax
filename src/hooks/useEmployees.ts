ec5f491fa50a2ee69219c23f50595e527ff1f36c
Saif Salem <saifsalemmail@gmail.com>
2024-09-11 10:04:19 +0300

    edit: updated some code to ensure good validation

--- src/hooks/useEmployees.ts
+++ src/hooks/useEmployees.ts
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   getAll,
   addOne,
@@ -7,21 +8,31 @@ import {
 import { Employee } from "../types";
 
 const useEmployees = () => {
-  const getEmployees = async () => await getAll();
+  const [isPending, setIsPending] = useState(false);
 
-  const addEmployee = async (employeeData: Employee) => {
-    if (
-      Object.values(employeeData).some(
-        (value) => !value || typeof value !== "string"
-      )
-    ) {
+  const getEmployees = async () => {
+    setIsPending(true);
+    const response = await getAll();
+    setIsPending(false);
+    return response;
+  };
+
+  const addEmployee = async (employeeData: {
+    name: string;
+    role: string;
+    department: string;
+  }) => {
+    setIsPending(true);
+    if (!employeeData.name || !employeeData.role || !employeeData.department) {
       throw new Error("All fields are required");
     }
     const response = await addOne(employeeData);
+    setIsPending(false);
     return response;
   };
 
   const updateEmployee = async (employeeId: string, employeeData: Employee) => {
+    setIsPending(true);
     if (
       Object.values(employeeData).some(
         (value) => !value || typeof value !== "string"
@@ -30,18 +41,22 @@ const useEmployees = () => {
       throw new Error("Data cannot be empty");
     }
     const response = await updateOne(employeeId, employeeData);
+    setIsPending(false);
     return response;
   };
 
   const deleteEmployee = async (employeeId: string) => {
+    setIsPending(true);
     if (!employeeId) {
       throw new Error("Employee ID cannot be empty");
     }
     const response = await deleteOne(employeeId);
+    setIsPending(false);
     return response;
   };
 
   return {
+    isPending,
     getEmployees,
     addEmployee,
     updateEmployee,
